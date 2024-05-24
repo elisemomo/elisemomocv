04737328c73cc8eee44085dd2888ec4f09e82b06
elisemomo <mukomelise@gmail.com>
2024-05-24 00:53:20 -0400

    Static folder and index.html added

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    @app.route('/ping')
+    def ping():
+        return "Pong!"
+
+    return app
